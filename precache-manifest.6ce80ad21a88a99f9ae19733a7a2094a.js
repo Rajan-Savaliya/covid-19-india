28e6bc821a3d48eed89b814cecc9a2afe7900885
rajan savaliya <rajanpatel99094@gmail.com>
2020-08-09 18:12:21 +0530

    Updates

---
+++ precache-manifest.6ce80ad21a88a99f9ae19733a7a2094a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c0f60fbd7416fb062ecd50e4b15509",
+    "url": "/covid-19-india/index.html"
+  },
+  {
+    "revision": "1e78bf0db538f7828007",
+    "url": "/covid-19-india/static/css/main.740874c4.chunk.css"
+  },
+  {
+    "revision": "bb3a8ba40f098dafc04a",
+    "url": "/covid-19-india/static/js/2.dfdb2bba.chunk.js"
+  },
+  {
+    "revision": "61e25812e7f326803ad264375c765013",
+    "url": "/covid-19-india/static/js/2.dfdb2bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e78bf0db538f7828007",
+    "url": "/covid-19-india/static/js/main.2111871d.chunk.js"
+  },
+  {
+    "revision": "5d668c84bf1c8488fa84",
+    "url": "/covid-19-india/static/js/runtime-main.a8e9ae23.js"
+  },
+  {
+    "revision": "1c8895dd79172b2f85043bf51fd5c7fa",
+    "url": "/covid-19-india/static/media/01.1c8895dd.svg"
+  },
+  {
+    "revision": "4e94bdfbeca8646b61a50920291ffaad",
+    "url": "/covid-19-india/static/media/02.4e94bdfb.svg"
+  },
+  {
+    "revision": "046acf9af57fab1131779a6afc365f0c",
+    "url": "/covid-19-india/static/media/03.046acf9a.svg"
+  },
+  {
+    "revision": "577bf8834560303fc0ebc011d8334d04",
+    "url": "/covid-19-india/static/media/04.577bf883.svg"
+  },
+  {
+    "revision": "6a1ae40a1db62466bd6bbda2cd4e581a",
+    "url": "/covid-19-india/static/media/05.6a1ae40a.svg"
+  },
+  {
+    "revision": "205f578ca91d90b22afa873d2faf3b5a",
+    "url": "/covid-19-india/static/media/06.205f578c.svg"
+  }
+]);
